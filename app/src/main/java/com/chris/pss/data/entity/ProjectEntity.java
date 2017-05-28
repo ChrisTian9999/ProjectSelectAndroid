383cb8a44c039574c0388d3e6b5f518eff669f45
ChrisTian <zhtian95@gmail.com>
2017-05-28 17:08:18 +0800

    加入专业的字段

--- app/src/main/java/com/chris/pss/data/entity/ProjectEntity.java
+++ app/src/main/java/com/chris/pss/data/entity/ProjectEntity.java
@@ -2,6 +2,7 @@ package com.chris.pss.data.entity;
 
 public class ProjectEntity {
     private int id;
+    private int majorId;
     private int departmentId;
     private int teacherId;
     private int studentId;
@@ -15,6 +16,24 @@ public class ProjectEntity {
     public ProjectEntity() {
     }
 
+    /**
+     * 创建课题的默认的构造函数
+     * @param departmentId 专业的id
+     * @param teacherId 教师的id
+     * @param title 标题
+     * @param detail 详情
+     * @param ranking 难度
+     */
+    public ProjectEntity(int departmentId, int majorId, int teacherId, String title, String detail, int ranking) {
+        this.departmentId = departmentId;
+        this.teacherId = teacherId;
+        this.majorId = majorId;
+        this.title = title;
+        this.detail = detail;
+        this.ranking = ranking;
+        this.isChecked = 0;
+        this.isFinish = 0;
+    }
 
     public int getId() {
         return id;
@@ -88,10 +107,19 @@ public class ProjectEntity {
         this.isFinish = isFinish;
     }
 
+    public int getMajorId() {
+        return majorId;
+    }
+
+    public void setMajorId(int majorId) {
+        this.majorId = majorId;
+    }
+
     @Override
     public String toString() {
         return "ProjectEntity{" +
                 "id=" + id +
+                ", majorId=" + majorId +
                 ", departmentId=" + departmentId +
                 ", teacherId=" + teacherId +
                 ", studentId=" + studentId +
